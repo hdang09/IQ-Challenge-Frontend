541b69499cdc69d9141aa9be6b90a48493f99a35
hdang09 <dangtranhai628@gmail.com>
2023-06-01 23:51:50 +0700

    FIX - Fix @ import v4

--- src/components/SIdebar/index.tsx
+++ src/components/SIdebar/index.tsx
@@ -17,35 +17,36 @@ const Sidebar = () => {
     let data = useContext(AnswersData);
 
     return (
-        <aside className={cn('wrapper')}>
-            <header className={cn('header')}>
-                <Image src={Logo} width={70} height={85} alt="F-Code Logo" />
-                <h2>F-Code</h2>
-            </header>
-            <div className={cn('item')}>
-                <h2>Thời gian</h2>
-                <div className={cn('timer')}>
-                    <Image src={ecliseBG} alt="Background timer" />
-                    <Image src={ecliseRunner} alt="Outline timer" />
-                    <div />
-                    <Timer />
-                </div>
-            </div>
-            <div className={cn('item')}>
-                <h2>Câu hỏi</h2>
-                <div className={cn('questions')}>
-                    {data.answers.map((item, idx) => (
-                        <Link href={`/challenge#${idx + 1}`} className={cn('question', item ? 'active' : '')} key={idx}>
-                            {++idx}
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            <div className={cn('item')}>
-                <Popup trigger={<Button>Nộp bài</Button>} />
-                <p>Kiểm tra bài làm kĩ trước khi nộp</p>
-            </div>
-        </aside>
+        <div>Hello</div>
+        // <aside className={cn('wrapper')}>
+        //     <header className={cn('header')}>
+        //         <Image src={Logo} width={70} height={85} alt="F-Code Logo" />
+        //         <h2>F-Code</h2>
+        //     </header>
+        //     <div className={cn('item')}>
+        //         <h2>Thời gian</h2>
+        //         <div className={cn('timer')}>
+        //             <Image src={ecliseBG} alt="Background timer" />
+        //             <Image src={ecliseRunner} alt="Outline timer" />
+        //             <div />
+        //             <Timer />
+        //         </div>
+        //     </div>
+        //     <div className={cn('item')}>
+        //         <h2>Câu hỏi</h2>
+        //         <div className={cn('questions')}>
+        //             {data.answers.map((item, idx) => (
+        //                 <Link href={`/challenge#${idx + 1}`} className={cn('question', item ? 'active' : '')} key={idx}>
+        //                     {++idx}
+        //                 </Link>
+        //             ))}
+        //         </div>
+        //     </div>
+        //     <div className={cn('item')}>
+        //         <Popup trigger={<Button>Nộp bài</Button>} />
+        //         <p>Kiểm tra bài làm kĩ trước khi nộp</p>
+        //     </div>
+        // </aside>
     );
 };
 
